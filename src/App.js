b94779acb693fc2aa141471be692b56899ab4b8b
ttahm3d <ttahm3d@gmail.com>
2022-03-14 15:45:35 +0530

    feat: created basic header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import { Header } from "./components/Header";
 import Router from "./router/Router";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Router />
     </BrowserRouter>
   );
