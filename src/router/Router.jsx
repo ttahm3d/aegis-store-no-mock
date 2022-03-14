ab876a2ad1bb4f50ff735727d8d7143f555503ad
ttahm3d <ttahm3d@gmail.com>
2022-03-14 18:40:19 +0530

    feat: added 404 fallback

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,6 +3,7 @@ import Cart from "../pages/cart/Cart";
 import Homepage from "../pages/homepage/Homepage";
 import Products from "../pages/products/listing/Listing";
 import Wishlist from "../pages/wishlist/Wishlist";
+import NotFound from "../components/NotFound/NotFound";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
       <Route path="/cart" element={<Cart />} />
       <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/products" element={<Products />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
